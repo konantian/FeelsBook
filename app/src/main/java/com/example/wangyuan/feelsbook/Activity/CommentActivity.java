10321c38cd51cc19fba325cecfde3dcbd4fd349a
Yuan Wang <wang17@ualberta.ca>
2018-12-28 12:02:22 -0700

    update button UI

--- app/src/main/java/com/example/wangyuan/feelsbook/Activity/CommentActivity.java
+++ app/src/main/java/com/example/wangyuan/feelsbook/Activity/CommentActivity.java
@@ -1,6 +1,7 @@
 package com.example.wangyuan.feelsbook.Activity;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
